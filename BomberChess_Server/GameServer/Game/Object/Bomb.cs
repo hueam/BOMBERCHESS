2312f9a772f9dc30378c9813b7fbd2c2f4a9c66f
hueam <zminsu5@gmail.com>
2024-06-18 14:12:54 +0900

    Init Project

---
+++ BomberChess_Server/GameServer/Game/Object/Bomb.cs
@@ -0,0 +1,44 @@
+﻿using Google.Protobuf.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer
+{
+    public class Bomb : GameObject
+    {
+        public GameObject Owner { get; set; }
+        public int BombDamage { get; set; }
+        public Bomb()
+        {
+            ObjectType = GameObjectType.Bomb;
+            BombDamage = 50;
+        }
+
+        public async void Update()
+        {
+            //  쏜 주인도 없고, 방도 없으면 나가기
+            if (Owner == null || Room == null) return;
+        }
+
+        public void ActiveBomb(Dictionary<int, Player> _players)
+        {
+            if (this == null) return;
+            List<Vector3> splashCells = this.GetSplashCellPos();
+            foreach (Player player in _players.Values)
+            {
+                Vector3 playerPos
+                    = new Vector3(player.Info.PosInfo.PosX, player.Info.PosInfo.PosY, player.Info.PosInfo.PosZ);
+                if (splashCells.Contains(playerPos))
+                {
+                    this.OnDamaged(this, player, BombDamage * player.Info.StatInfo.Atk);
+                }
+
+                Room.LeaveGame(this.Id);
+            }
+        }
+    }
+}
---
+++ BomberChess_Server/GameServer/Game/Object/Player.cs
@@ -0,0 +1,37 @@
+﻿
+using GameServer.Session;
+using Google.Protobuf.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer
+{
+    public class Player : GameObject
+    {
+        public ClientSession Session { get; set; }  // 내가 어느 세션에 속해있는가?
+
+        public Player()
+        {   // Protocol.proto의 Enum
+            ObjectType = GameObjectType.Player;
+            //Info.Hp = 100;
+            Level = 1;
+            MaxHp = 100;
+            Hp = MaxHp;
+            Atk = 2;
+            Speed = 10.0f;
+        }
+        public override void OnDamaged(GameObject attacker, GameObject fired, int damage)
+        {
+            base.OnDamaged(attacker, fired, damage);
+        }
+        public override void OnDead(GameObject attacker)
+        {
+            base.OnDead(attacker);
+        }
+    }
+
+}
